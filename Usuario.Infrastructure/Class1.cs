d76f6428a98261e3b815c5342c6bafc85f9639d9
VANESSA DA CUNHA LOPES <vanessa.lopes@alunos.sc.senac.br>
2022-05-11 21:45:55 -0300

    Aula

---
+++ Usuario.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Usuario.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ Usuario/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Usuario.Application.Interfaces;
+
+namespace Usuario.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _usuarioService.Get());
+        }
+
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await _usuarioService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] InputUsuarioDto usuarioDto)
+        {
+            _usuarioService.Add(usuarioDto.Login, usuarioDto.Password);
+
+            return Ok();
+        }
+    }
+}
